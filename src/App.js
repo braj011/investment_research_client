d7c15d0c63f488c231cf92091224e6976c7069cd
braj011 <bhagyaraj01@gmail.com>
2018-11-21 21:16:18 +0000

    started client side, got initial news API to work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
+// import { Route, withRouter, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    news: []
+  }
+
+componentDidMount() {
+  this.getNewsHeadlines()
+}
+ 
+  // move to API class 
+  getNewsHeadlines = () => {
+    if (this.state.news.length > 0) {
+      this.setState({ news: []})
+    }
+    return fetch('http://localhost:3000/api/v1/news_apis/')
+      .then(resp => resp.json())
+
+      .then(newsData => {
+        const news = newsData.articles.map(article => {
+          if (!article.author) {
+            return {...article, author: "Unknown"}
+          } else {
+            return article
+          }
+        })
+        this.setState({news: news})
+      }) 
+  } 
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} className="App-logo " alt="logo" />
+        <ul> 
+          {this.state.news.map(article => <li>{article.author} </li>)}
+        </ul> 
       </div>
-    );
+    )
   }
 }
 
