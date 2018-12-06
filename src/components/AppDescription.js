fd1a8e69eeba24a357a954bdbce029285d9be7de
braj011 <bhagyaraj01@gmail.com>
2018-12-06 14:49:01 +0000

    final styling pre science fair

--- src/components/AppDescription.js
+++ src/components/AppDescription.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import { Segment } from 'semantic-ui-react'
+
 class AppDescription extends Component {
 
   render() {
     return(
-      <div> DESCRIPTION CONTAINER (TBD)
-      </div> 
+      <Segment color="blue"> 
+        <h1 className="welcome-header">Welcome to stockNote. </h1>
+        <p></p>
+        The amateur investment research platform where you can get custom news on your portfolio as well as add notes - so you can keep track of your thinking and analysis.
+        <p>Create email alerts and updates so you're always on top of your game!</p>
+      </Segment> 
     )
   }
 
